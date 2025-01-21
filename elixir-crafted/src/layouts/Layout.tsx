9bde28d21f6477d9bbf21c643bbeef9e9f605abf
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-20 22:05:12 -0600

    fixing error

--- elixir-crafted/src/layouts/Layout.tsx
+++ elixir-crafted/src/layouts/Layout.tsx
@@ -1,9 +1,11 @@
-import { Outlet } from "react-router-dom"
-import Header from "../components/Header"
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
 
 export default function Layout() {
   return (
-    <Header />
-    <Outlet />
-  )
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
 }
