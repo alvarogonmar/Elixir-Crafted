c273021c476e224e0690ea2890c6b40b5cc39d4a
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-20 21:44:28 -0600

    img logo added

--- elixir-crafted/src/components/Header.tsx
+++ elixir-crafted/src/components/Header.tsx
@@ -2,7 +2,9 @@ export default function Header() {
   return (
     <header className="bg-slate-800">
       <div className="mx-auto container px-5 py-16">
-        <div className="flex justify-between items-center"></div>
+        <div className="flex justify-between items-center">
+          <img src="/logo.svg" alt="logo" className="w-32" />
+        </div>
 
         <nav></nav>
       </div>
