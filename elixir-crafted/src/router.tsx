47c0ddad255d3a6f371b9ac58e07e0662538b9a2
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-20 22:07:54 -0600

    adding layout and fixing error in components

--- elixir-crafted/src/router.tsx
+++ elixir-crafted/src/router.tsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import IndexPage from "./views/IndexPage";
 import FavoritesPage from "./views/FavoritesPage";
+import Layout from "./layouts/Layout";
 
 export default function AppRouter() {
   return (
     // Es el principal y tiene que rodear todos los componentes
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<IndexPage />}></Route>
-        <Route path="/favorites" element={<FavoritesPage />}></Route>
+        <Route element={<Layout />}>
+          <Route path="/" element={<IndexPage />}></Route>
+          <Route path="/favorites" element={<FavoritesPage />}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- elixir-crafted/src/views/IndexPage.tsx
+++ elixir-crafted/src/views/IndexPage.tsx
@@ -1,7 +1,7 @@
 export default function IndexPage() {
   return (
     <>
-      <h1>Favorites</h1>
+      <h1>Home</h1>
     </>
   );
 }
