4f666be68ac8a71b7bb30d516c88fd6924b48765
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-20 21:42:04 -0600

    header css added

--- elixir-crafted/src/components/Header.tsx
+++ elixir-crafted/src/components/Header.tsx
@@ -1,3 +1,11 @@
 export default function Header() {
-  return <header></header>;
+  return (
+    <header className="bg-slate-800">
+      <div>
+        <div></div>
+
+        <nav></nav>
+      </div>
+    </header>
+  );
 }
