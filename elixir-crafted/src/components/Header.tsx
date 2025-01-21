50ffd36792150294be429a688eda6d436a06ee95
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-20 22:19:38 -0600

    using link to navegate across pages

--- elixir-crafted/src/components/Header.tsx
+++ elixir-crafted/src/components/Header.tsx
@@ -1,12 +1,22 @@
+import { Link, NavLink } from "react-router-dom";
 export default function Header() {
   return (
     <header className="bg-slate-800">
       <div className="mx-auto container px-5 py-16">
         <div className="flex justify-between items-center">
-          <img src="/logo.svg" alt="logo" className="w-32" />
-        </div>
+          <div>
+            <img src="/logo.svg" alt="logo" className="w-32" />
+          </div>
 
-        <nav></nav>
+          <nav className="flex gap-4">
+            <Link to="/" className="text-white uppercase font-bold">
+              Home
+            </Link>
+            <Link to="/favorites" className="text-white uppercase font-bold">
+              Favorites
+            </Link>
+          </nav>
+        </div>
       </div>
     </header>
   );
