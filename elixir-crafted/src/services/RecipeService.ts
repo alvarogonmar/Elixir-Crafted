70774cdf8327f2fb01d5d53c04cc087513cb7074
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-21 21:01:42 -0600

    adding data

--- elixir-crafted/src/services/RecipeService.ts
+++ elixir-crafted/src/services/RecipeService.ts
@@ -3,4 +3,6 @@ import { CategoriesAPIResponseSchema } from "../schemas/recipes-schema"
 
 export async function getCategories() {
     const url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list"
+    const {data} = await axios(url)
+    console.log(data)
 }
