0ca57329484e6785565dcc62ed294247dd89acae
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-21 21:02:54 -0600

    validation added with safeParse

--- elixir-crafted/src/services/RecipeService.ts
+++ elixir-crafted/src/services/RecipeService.ts
@@ -4,5 +4,7 @@ import { CategoriesAPIResponseSchema } from "../schemas/recipes-schema"
 export async function getCategories() {
     const url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list"
     const {data} = await axios(url)
-    console.log(data)
+    const result = CategoriesAPIResponseSchema.safeParse(data)
+
+    console.log(result)
 }
