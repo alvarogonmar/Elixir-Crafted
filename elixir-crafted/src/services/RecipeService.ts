92b8a1bd8d0fd523c3e18e9ff373224d9fc72365
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-21 22:40:10 -0600

    function getRecipes added

--- elixir-crafted/src/services/RecipeService.ts
+++ elixir-crafted/src/services/RecipeService.ts
@@ -1,5 +1,6 @@
 import axios from "axios"
 import { CategoriesAPIResponseSchema } from "../schemas/recipes-schema"
+import { SearchFilter } from "../types"
 
 export async function getCategories() {
     const url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list"
@@ -9,4 +10,8 @@ export async function getCategories() {
     if(result.success) {
         return result.data
     }
+}
+
+export async function getRecipes(searchFilters:SearchFilter) {
+    console.log(searchFilters)
 }
