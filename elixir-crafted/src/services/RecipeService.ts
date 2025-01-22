b2bfd2b74239370a57e6de3935481848ba38ac23
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-21 21:00:18 -0600

    Url API added

--- elixir-crafted/src/services/RecipeService.ts
+++ elixir-crafted/src/services/RecipeService.ts
@@ -1,6 +1,6 @@
-
+import axios from "axios"
+import { CategoriesAPIResponseSchema } from "../schemas/recipes-schema"
 
 export async function getCategories() {
-    
-    console.log("from recipe")
+    const url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list"
 }
