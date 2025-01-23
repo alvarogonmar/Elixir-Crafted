7af65d5810dfd071fc040c676ea993f0e42ff8e6
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-23 17:48:33 -0600

    added drink img and text

--- elixir-crafted/src/components/DrinkCard.tsx
+++ elixir-crafted/src/components/DrinkCard.tsx
@@ -5,5 +5,20 @@ type DrinkCardProps = {
 };
 
 export default function DrinkCard({ drink }: DrinkCardProps) {
-  return <h2>{drink.strDrink}</h2>;
+  return (
+    <div className="border shadow-lg">
+      <div>
+        <img src={drink.strDrinkThumb} alt={`${drink.strDrink} image`} />
+      </div>
+      <div className="p-5">
+        <h2 className="text-2xl truncate font-black">{drink.strDrink}</h2>
+        <button
+          type="button"
+          className="bg-orange-400 hover:bg-orange-500 mt-5 w-full p-3 font-bold text-white text-lg"
+        >
+          See Recipe
+        </button>
+      </div>
+    </div>
+  );
 }
