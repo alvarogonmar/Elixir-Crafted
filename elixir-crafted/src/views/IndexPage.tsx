20efdfe867d2a4733bb2b6bcf0bc5311c48edce1
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-22 19:59:11 -0600

    added hasDrinks function

--- elixir-crafted/src/views/IndexPage.tsx
+++ elixir-crafted/src/views/IndexPage.tsx
@@ -1,11 +1,15 @@
+import { useMemo } from "react";
 import { useAppStore } from "../stores/useAppStore";
 
 export default function IndexPage() {
   const drinks = useAppStore((state) => state.drinks);
 
+  const hasDrinks = useMemo(() => drinks.drinks.length > 0, [drinks]);
+
   return (
     <>
-      <h1>Home</h1>
+      <h1 className="text-6xl font-extrabold">Recipes</h1>
+      {hasDrinks}
     </>
   );
 }
