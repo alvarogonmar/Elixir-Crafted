7248a905a71707083e2fcc8c08eba823b6cdbaf3
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-23 18:12:45 -0600

    adding styles

--- elixir-crafted/src/views/IndexPage.tsx
+++ elixir-crafted/src/views/IndexPage.tsx
@@ -10,17 +10,19 @@ export default function IndexPage() {
   return (
     <>
       <h1 className="text-6xl font-extrabold">Recipes</h1>
-      {hasDrinks ? (
-        <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-10 gap-10">
-          {drinks.drinks.map((drink) => (
-            <DrinkCard key={drink.idDrink} drink={drink} />
-          ))}
-        </div>
-      ) : (
-        <p className="my-10 text-center text-2xl">
-          There are no recipes yet, use the form
-        </p>
-      )}
+      <div className="container mx-auto">
+        {hasDrinks ? (
+          <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 my-10 gap-10">
+            {drinks.drinks.map((drink) => (
+              <DrinkCard key={drink.idDrink} drink={drink} />
+            ))}
+          </div>
+        ) : (
+          <p className="my-10 text-center text-2xl">
+            There are no recipes yet, use the form
+          </p>
+        )}
+      </div>
     </>
   );
 }
