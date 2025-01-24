4cd571ff9c3fa71242c98ae17686b0c9bf0254bb
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-23 22:18:19 -0600

    added FavoritesSliceType

--- elixir-crafted/src/stores/useAppStore.ts
+++ elixir-crafted/src/stores/useAppStore.ts
@@ -3,6 +3,7 @@ import { devtools } from "zustand/middleware";
 import { createRecipesSlice, RecipesSliceType } from "./recipeSlice";
 import { FavoritesSliceType, createFavoritesSlice } from "./favoritesSlice";
 
-export const useAppStore = create<RecipesSliceType>()(devtools((...a) => ({
-    ...createRecipesSlice(...a)
+export const useAppStore = create<RecipesSliceType & FavoritesSliceType>()(devtools((...a) => ({
+    ...createRecipesSlice(...a),
+    ...createFavoritesSlice(...a)
 })))
