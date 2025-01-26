dff2619377092d4909f144497a0292fb89da43d6
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-25 22:04:37 -0600

    return color to hover

--- elixir-crafted/src/components/Modal.tsx
+++ elixir-crafted/src/components/Modal.tsx
@@ -104,7 +104,7 @@ export default function Modal() {
                     <button
                       type="button"
                       className="w-full rounded bg-orange-600 p-3 
-                      font-bold uppercase shadow text-white hover:bg-orange-600"
+                      font-bold uppercase shadow text-white hover:bg-orange-500"
                       onClick={() => handleAddToFavorites()}
                     >
                       Add to Favorites
