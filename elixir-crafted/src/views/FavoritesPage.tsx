97da4e9a280794b36bc6d566ba2c9b63bfcb6d6b
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-28 22:38:13 -0600

    adding favorites from useappstore

--- elixir-crafted/src/views/FavoritesPage.tsx
+++ elixir-crafted/src/views/FavoritesPage.tsx
@@ -1,4 +1,7 @@
+import { useAppStore } from "../stores/useAppStore";
+
 export default function FavoritesPage() {
+  const favorites = useAppStore((state) => state.favorites);
   return (
     <>
       <h1>Favorites</h1>
