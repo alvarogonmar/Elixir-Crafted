d5dfe2fcf0f88ace8318a5d729c8ee772966e03f
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-28 22:01:41 -0600

    adding  a function to comprobate if the recipe is in favorites

--- elixir-crafted/src/stores/favoritesSlice.ts
+++ elixir-crafted/src/stores/favoritesSlice.ts
@@ -6,9 +6,17 @@ export type FavoritesSliceType = {
     handleClickFavorite: (recipe: Recipe) => void
 }
 
-export const createFavoritesSlice : StateCreator<FavoritesSliceType> = () => ({
+export const createFavoritesSlice : StateCreator<FavoritesSliceType> = (set, get) => ({
     favorites: [],
     handleClickFavorite: (recipe) => {
-        console.log(recipe)
+        if(get().favorites.some(favorite => favorite.idDrink === recipe.idDrink)){
+            console.log("si")
+        } else {
+            console.log("no")
+
+            set((state) => ({
+                favorites: [...state.favorites, recipe]
+            }))
+        }
     }
 })
