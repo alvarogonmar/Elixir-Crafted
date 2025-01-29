2d2905a2098c0d4b78ca80e2301534fe6798d0c9
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-28 22:08:41 -0600

    adding function delete when you already have the recipe

--- elixir-crafted/src/stores/favoritesSlice.ts
+++ elixir-crafted/src/stores/favoritesSlice.ts
@@ -10,7 +10,9 @@ export const createFavoritesSlice : StateCreator<FavoritesSliceType> = (set, get
     favorites: [],
     handleClickFavorite: (recipe) => {
         if(get().favorites.some(favorite => favorite.idDrink === recipe.idDrink)){
-            console.log("si")
+            set((state) => ({
+                favorites: state.favorites.filter( favorite => favorite.idDrink !== recipe.idDrink)
+            }))
         } else {
             console.log("no")
 
