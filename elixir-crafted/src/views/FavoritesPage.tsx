5ce0e566fc536ceee31425ab95f49bcc5eeb39d0
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-28 22:43:19 -0600

    showing favorites in favorites page

--- elixir-crafted/src/views/FavoritesPage.tsx
+++ elixir-crafted/src/views/FavoritesPage.tsx
@@ -1,10 +1,17 @@
+import DrinkCard from "../components/DrinkCard";
 import { useAppStore } from "../stores/useAppStore";
 
 export default function FavoritesPage() {
   const favorites = useAppStore((state) => state.favorites);
   return (
     <>
-      <h1>Favorites</h1>
+      <h1 className="text-6xl font-extrabold">Favorites</h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 my-10 gap-10">
+        {favorites.map((drink) => (
+          <DrinkCard key={drink.idDrink} drink={drink} />
+        ))}
+      </div>
     </>
   );
 }
