fdfb94994336270c64314261e374ee3510ae3c48
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-28 22:53:30 -0600

    notificaitons slice

---
+++ elixir-crafted/src/stores/notificationSlice.ts
@@ -0,0 +1,9 @@
+import { StateCreator } from "zustand";
+
+export type NotificationSliceType = {
+
+}
+
+export const createFavoritesSlice : StateCreator<NotificationSliceType> = (set, get, api) => ({
+    
+})
