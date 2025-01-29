a775b61e8538049d1dceb240df13ec7ef4f28a84
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-29 17:06:02 -0600

    addng notification

--- elixir-crafted/src/layouts/Layout.tsx
+++ elixir-crafted/src/layouts/Layout.tsx
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import Modal from "../components/Modal";
 import { useAppStore } from "../stores/useAppStore";
 import { useEffect } from "react";
+import Notification from "../components/Notification";
 
 export default function Layout() {
   const loadFromStorage = useAppStore((state) => state.loadFromStorage);
@@ -17,6 +18,7 @@ export default function Layout() {
       </main>
 
       <Modal />
+      <Notification />
     </>
   );
 }
