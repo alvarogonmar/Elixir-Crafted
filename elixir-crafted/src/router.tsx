11493a376f7339bc01dee1ef5c65676d105780bc
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-29 23:23:54 -0600

    added react router dom to index page

--- elixir-crafted/src/router.tsx
+++ elixir-crafted/src/router.tsx
@@ -1,16 +1,24 @@
 import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import IndexPage from "./views/IndexPage";
 import Layout from "./layouts/Layout";
-const FavoritesPage = lazy(() => import("./views/FavoritesPage"));
 
+const IndexPage = lazy(() => import("./views/IndexPage"));
+const FavoritesPage = lazy(() => import("./views/FavoritesPage"));
 export default function AppRouter() {
   return (
     // Es el principal y tiene que rodear todos los componentes
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<IndexPage />} index></Route>
+          <Route
+            path="/"
+            element={
+              <Suspense fallback="Loading...">
+                <IndexPage />
+              </Suspense>
+            }
+            index
+          ></Route>
           <Route
             path="/favorites"
             element={
