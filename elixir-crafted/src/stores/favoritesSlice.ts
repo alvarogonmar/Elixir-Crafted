f205d4c61b2b9012a05c04682698d0213e56da2d
alvarogonmar <alvarogonmar0@gmail.com>
2025-01-29 19:22:40 -0600

    added function at if

--- elixir-crafted/src/stores/favoritesSlice.ts
+++ elixir-crafted/src/stores/favoritesSlice.ts
@@ -1,5 +1,6 @@
 import { StateCreator } from "zustand";
 import { Recipe } from "../types";
+import { createNotificationSlice } from "./notificationSlice";
 
 export type FavoritesSliceType = {
     favorites: Recipe[]
@@ -15,10 +16,12 @@ export const createFavoritesSlice : StateCreator<FavoritesSliceType> = (set, get
             set((state) => ({
                 favorites: state.favorites.filter( favorite => favorite.idDrink !== recipe.idDrink)
             }))
+            createNotificationSlice().showNotification({text: "Removed from Favorites", error: false})
         } else {
             set((state) => ({
                 favorites: [...state.favorites, recipe]
             }))
+            createNotificationSlice().showNotification({text: "Added to Favorites", error: false})
         }
         localStorage.setItem("favorites", JSON.stringify(get().favorites))
     },
